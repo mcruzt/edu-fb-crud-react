ef72d465e18a39d3c22df38b16e1f93681c71aa2
Maribel Cruz Tellez <maribel.cruz.tellez@gmail.com>
2020-09-22 20:36:13 -0500

    Updates

---
+++ precache-manifest.224b8e117769bfd92ff0dfef1ad371ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f705bed7e3492dce8b86434d5da1d4",
+    "url": "/edu-fb-crud-react/index.html"
+  },
+  {
+    "revision": "6a7551e83494f0238eb4",
+    "url": "/edu-fb-crud-react/static/css/2.c88bba55.chunk.css"
+  },
+  {
+    "revision": "54e01cf942710c3bc19c",
+    "url": "/edu-fb-crud-react/static/css/main.3c076489.chunk.css"
+  },
+  {
+    "revision": "6a7551e83494f0238eb4",
+    "url": "/edu-fb-crud-react/static/js/2.727f29cd.chunk.js"
+  },
+  {
+    "revision": "659df5b420806b55437f4f9e57f60411",
+    "url": "/edu-fb-crud-react/static/js/2.727f29cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54e01cf942710c3bc19c",
+    "url": "/edu-fb-crud-react/static/js/main.39a9c8fd.chunk.js"
+  },
+  {
+    "revision": "cbce013ffca4c57e89d0",
+    "url": "/edu-fb-crud-react/static/js/runtime-main.8fbee398.js"
+  }
+]);
